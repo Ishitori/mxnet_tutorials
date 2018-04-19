84953402cd037b0204b417cba291d9c7c0527fc1
Indu Bharathi <indhubharathi@gmail.com>
2018-04-19 09:21:11 +0000

    Polishing guided_backprop

---
+++ visualize_cnn/utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+def save_gradient_images(gradient, file_name):
+    gradient = gradient - gradient.min()
+    gradient /= gradient.max()
+    gradient = np.uint8(gradient * 255).transpose(1, 2, 0)
+    path_to_file = file_name + '.jpg'
+    # Convert RBG to GBR
+    gradient = gradient[..., ::-1]
+    cv2.imwrite(path_to_file, gradient)
+
+def convert_to_grayscale(cv2im):
+    grayscale_im = np.sum(np.abs(cv2im), axis=0)
+    im_max = np.percentile(grayscale_im, 99)
+    im_min = np.min(grayscale_im)
+    grayscale_im = (np.clip((grayscale_im - im_min) / (im_max - im_min), 0, 1))
+    grayscale_im = np.expand_dims(grayscale_im, axis=0)
+    return grayscale_im
+
+def get_positive_negative_saliency(gradient):
+    pos_saliency = (np.maximum(0, gradient) / gradient.max())
+    neg_saliency = (np.maximum(0, -gradient) / -gradient.min())
+    return pos_saliency, neg_saliency
+
