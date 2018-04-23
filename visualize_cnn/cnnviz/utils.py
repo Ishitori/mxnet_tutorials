4a9322bbdcde539643949c868ed27346761ccdaf
Indu Bharathi <indhubharathi@gmail.com>
2018-04-23 21:08:36 +0000

    Utils

---
+++ visualize_cnn/cnnviz/utils.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+
+def save_gradient_images(gradient, file_name):
+    gradient = gradient - gradient.min()
+    gradient /= gradient.max()
+    gradient = np.uint8(gradient * 255).transpose(1, 2, 0)
+    path_to_file = file_name + '.jpg'
+    # Convert RBG to GBR
+    gradient = gradient[..., ::-1]
+    cv2.imwrite(path_to_file, gradient)
+
+def convert_to_grayscale(cv2im):
+    grayscale_im = np.sum(np.abs(cv2im), axis=0)
+    im_max = np.percentile(grayscale_im, 99)
+    im_min = np.min(grayscale_im)
+    grayscale_im = (np.clip((grayscale_im - im_min) / (im_max - im_min), 0, 1))
+    grayscale_im = np.expand_dims(grayscale_im, axis=0)
+    return grayscale_im
+
+def get_positive_negative_saliency(gradient):
+    pos_saliency = (np.maximum(0, gradient) / gradient.max())
+    neg_saliency = (np.maximum(0, -gradient) / -gradient.min())
+    return pos_saliency, neg_saliency
+
+def save_class_activation_on_image(org_img, activation_map, file_name):
+
+    # Grayscale activation map
+    path_to_file = file_name+'_Cam_Grayscale.jpg'
+    cv2.imwrite(path_to_file, activation_map)
+    # Heatmap of activation map
+    activation_heatmap = cv2.applyColorMap(activation_map, cv2.COLORMAP_HSV)
+    path_to_file = file_name+'_Cam_Heatmap.jpg'
+    cv2.imwrite(path_to_file, activation_heatmap)
+    # Heatmap on picture
+    org_img = cv2.resize(org_img, (224, 224))
+
+    img_with_heatmap = np.float32(activation_heatmap) + np.float32(org_img)
+    img_with_heatmap = img_with_heatmap / np.max(img_with_heatmap)
+    path_to_file = file_name+'_Cam_On_Image.jpg'
+    
+    to_write = np.uint8(255 * img_with_heatmap)
+    cv2.imwrite(path_to_file, to_write)
+
