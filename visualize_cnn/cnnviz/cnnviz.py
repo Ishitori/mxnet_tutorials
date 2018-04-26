763c3ad9c0b330897236efd76777ee3446f5975f
Indu Bharathi <indhubharathi@gmail.com>
2018-04-26 00:15:39 +0000

    All visualization code moved to cnnviz/cnnviz.py. Minimal.ipynb shows how to just call the functions and visualize

--- visualize_cnn/cnnviz/cnnviz.py
+++ visualize_cnn/cnnviz/cnnviz.py
@@ -1,6 +1,8 @@
 import mxnet as mx
 from mxnet import autograd
+
 import numpy as np
+import cv2
 
 from cnnviz.layers import Conv2D
 
@@ -33,7 +35,6 @@ def _get_grad(net, image, class_id=None, conv_layer_name=None, image_grad=False)
         conv_out = Conv2D.conv_output
         return conv_out[0].asnumpy(), conv_out.grad[0].asnumpy()
 
-
 def get_conv_out_grad(net, image, class_id=None, conv_layer_name=None):
     return _get_grad(net, image, class_id, conv_layer_name, image_grad=False)
 
@@ -46,4 +47,54 @@ def grad_to_image(gradient):
     gradient = np.uint8(gradient * 255).transpose(1, 2, 0)
     gradient = gradient[..., ::-1]
     return gradient
+
+def get_cam(imggrad, conv_out):
+    weights = np.mean(imggrad, axis=(1, 2))
+
+    cam = np.ones(conv_out.shape[1:], dtype=np.float32)
+    for i, w in enumerate(weights):
+        cam += w * conv_out[i, :, :]
+
+    cam = cv2.resize(cam, (imggrad.shape[1], imggrad.shape[2]))
+    cam = np.maximum(cam, 0)
+    cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam)) 
+    cam = np.uint8(cam * 255)
+    return cam
+
+def get_guided_grad_cam(cam, imggrad):
+    return np.multiply(cam, imggrad)
+
+def get_img_heatmap(orig_img, activation_map):
+    heatmap = cv2.applyColorMap(activation_map, cv2.COLORMAP_HSV)
+    orig_img = cv2.resize(orig_img, (orig_img.shape[0], orig_img.shape[1]))
+
+    img_heatmap = np.float32(heatmap) + np.float32(orig_img)
+    img_heatmap = img_heatmap / np.max(img_heatmap)
+
+    return img_heatmap
+
+def to_grayscale(cv2im):
+    grayscale_im = np.sum(np.abs(cv2im), axis=0)
+    im_max = np.percentile(grayscale_im, 99)
+    im_min = np.min(grayscale_im)
+    grayscale_im = (np.clip((grayscale_im - im_min) / (im_max - im_min), 0, 1))
+    grayscale_im = np.expand_dims(grayscale_im, axis=0)
+    return grayscale_im
+
+def visualize_class_activation(net, preprocessed_img, orig_img, conv_layer_name):
+    # Returns grad-cam heatmap, guided grad-cam, guided grad-cam saliency
+    imggrad = get_image_grad(net, preprocessed_img)
+    conv_out, conv_out_grad = get_conv_out_grad(net, preprocessed_img, conv_layer_name=conv_layer_name)
+
+    cam = get_cam(imggrad, conv_out)
+    
+    ggcam = get_guided_grad_cam(cam, imggrad)
+    img_ggcam = grad_to_image(ggcam)
+    
+    img_heatmap = get_img_heatmap(orig_img, cam)
+    
+    ggcam_gray = to_grayscale(ggcam)
+    img_ggcam_gray = np.squeeze(grad_to_image(ggcam_gray))
+    
+    return img_heatmap, img_ggcam, img_ggcam_gray
 
